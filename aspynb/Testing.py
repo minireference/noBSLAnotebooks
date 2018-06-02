4ee78a6e88cc1ac687abd68876d9d6802f95e0a7
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-02 16:34:23 -0400

    testing

---
+++ aspynb/Testing.py
@@ -0,0 +1,70 @@
+def cells():
+    # setup SymPy
+    from sympy import *
+    x, y, z, t = symbols('x y z t')
+    init_printing()
+    
+    # setup plotting
+    %matplotlib notebook
+    import matplotlib.pyplot as mpl
+    from util.plot_helpers import plot_vec, plot_vecs, autoscale_arrows
+
+    '''
+    '''
+
+    simplify(sin(2*x)*cos(2*x))
+
+    '''
+    '''
+
+    a = Matrix([1,1,1])
+    a.
+
+    '''
+    '''
+
+    S(1)/3
+
+    '''
+    '''
+
+    type(1)
+
+    '''
+    '''
+
+    type(S(1))
+
+    '''
+    '''
+
+    float(S(1)/3)
+
+    '''
+    '''
+
+    N(S(1)/3)
+
+    '''
+    '''
+
+    pi.n(100)
+    type(pi.n(100))
+
+    '''
+    '''
+
+    e
+
+    '''
+    '''
+
+    E
+
+    '''
+    '''
+
+
+    '''
+    '''
+
