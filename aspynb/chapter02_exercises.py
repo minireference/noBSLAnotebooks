ec9a1e3f44a147060df5b7d33675a44bbee6d99f
Ivan Savov <ivan.savov@gmail.com>
2018-06-17 13:19:54 -0400

    used md list helper to generate E2.? notebook

---
+++ aspynb/chapter02_exercises.py
@@ -0,0 +1,240 @@
+def cells():
+    '''
+    # 2/ Intro to linear algebra
+    '''
+
+    '''
+    '''
+
+    # setup SymPy
+    from sympy import *
+    x, y, z, t = symbols('x y z t')
+    init_printing()
+    
+    # setup plotting
+    %matplotlib notebook
+    import matplotlib.pyplot as mpl
+    from util.plot_helpers import plot_vec, plot_vecs, autoscale_arrows
+    from util.plot_helpers import plot_point, plot_line, plot_plane
+    from util.plot_helpers import plot_augmat
+    
+    # define alias Vector for Matrix objects
+    Vector = Matrix
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ## Definitions
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.1
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.2
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.3
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ## Vector operations
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.4
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.5
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.6 
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ## Matrix operations
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.7
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.8
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.9
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ## Linearity
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+    '''
+    ### E2.10
+    '''
+
+    '''
+    '''
+
+
+    '''
+    '''
+
+
+    '''
+    '''
+
